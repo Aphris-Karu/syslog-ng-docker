394e4cc3422a01269d116af5f77410f3258faa38
Aphris-Karu <46301934+Aphris-Karu@users.noreply.github.com>
2020-04-20 13:30:25 -0500

    Adding ENV for when to rotate logs.

--- syslog-ng/start-syslog-ng.sh
+++ syslog-ng/start-syslog-ng.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
-if [[ $LOGROTATE = "Yes" || $LOGROTATE = "yes" || $LOGROTATE -eq 1 ]]
+shopt -s nocasematch
+if [[ $LOGROTATE = "yes" || $LOGROTATE -eq 1 ]]
   then
     service cron start
-  fi
+  case $ROTATE_WHEN in
+      hour | hourly)
+       cp /srv/logrotate /etc/cron.hourly/logrotate ;;
+      day | daily)
+       cp /srv/logrotate /etc/cron.daily/logrotate ;;
+      week | weekly)
+       cp /srv/logrotate /etc/cron.weekly/logrotate ;;
+      month | monthly)
+       cp /srv/logrotate /etc/cron.monthly/logrotate ;;
+      *)
+        service cron stop
+        echo "Rotation not set"
+      ;;
+   esac
+ fi
 
 /usr/sbin/syslog-ng -F
